730db2281f4cfacdd74b6a6b4a2681b7e35dfd5a
Juergen E. Fischer <jef@norbit.de>
2026-08-11 15:33:52 +0200

    python updates: * python3-affine: add attrs dependency * python3-blosc2: replace requests with httpx * python3-ipython: remove decorator dependency * python3-mapproxy: add multiprocess dependency * python3-python-discovery: remove platformdirs dependency * add python3-multiprocess for mapproxy * add python3-dill for multiprocess

--- src/python3-python-discovery/osgeo4w/package.sh
+++ src/python3-python-discovery/osgeo4w/package.sh
@@ -2,7 +2,7 @@ export P=python3-python-discovery
 export V=pip
 export B=pip
 export MAINTAINER=JuergenFischer
-export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-filelock python3-platformdirs"
+export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-filelock"
 export PACKAGES="python3-python-discovery"
 
 source ../../../scripts/build-helpers
