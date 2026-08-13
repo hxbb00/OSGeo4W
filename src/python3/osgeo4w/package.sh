5ec6f7ed36632a8cfc6890f3b13ce2c56854a83c
Juergen E. Fischer <jef@norbit.de>
2026-08-13 19:11:52 +0200

    update python3 to 3.12.14 (and drop patches applied upstream)

--- src/python3/osgeo4w/package.sh
+++ src/python3/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=python3
-export V=3.12.13
+export V=3.12.14
 export B="next $P-core"
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="openssl-devel bzip2-devel xz-devel zlib-devel sqlite3-devel"
